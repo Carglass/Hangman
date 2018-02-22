cce6b9f5b6d1d5be379129c76a4f74f11c701471
Maxime Burlat <maximeburlat@gmail.com>
2018-02-22 01:05:19 -0600

    Functional game with html, need to reset html at the reset of game do

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// initializing the global variables
 let gameIsOn = false;
 let word = "BONJOUR";
 let count = 10;
@@ -7,25 +8,39 @@ var arrayToComplete = new Array(word.length);
     }
 let answer = new String();
 
+// hasGameEnded test if the game is over and terminates it if necessary
 function hasGameEnded(globalCount) {
     if (globalCount >= 0 && answer === word) {
         console.log("You won!");
+        displayScore('win');
         gameIsOn = false;
     } else if (globalCount === 0){
         console.log("You lost :(");
+        displayScore('loose')
         gameIsOn = false;
     } else {
         gameIsOn = true;
     }
 }
 
+function displayScore(score){
+    elem = document.getElementById("score");
+    if (score === "win") {
+        elem.innerHTML = "You won ! Press any key to restart";
+    } else if (score === "loose") {
+        elem.innerHTML = "You lost :( Press any key to restart";
+    } else {
+        elem.innerHTML = "An error ocurred :/";
+    }
+}
+
+// treatLetter takes a letter as an input and finds if it is in the word, then complete the matching table accordingly
 function treatLetter(input) {
     letter = input.toUpperCase();
     console.log(letter);
     var newRegular = RegExp(letter);
     console.log(newRegular);
     if (word.includes(letter)) {
-        // alert("found it!");
         indexToStart = 0;
         stringToSearch = word;
         while ((indexToStart < word.length) && (stringToSearch.includes(letter))) {
@@ -44,6 +59,7 @@ function treatLetter(input) {
         }
         console.log(wordToComplete);
         answer = wordToComplete;
+        displayAnswer();
     } else {
         console.log("missed");
     }
@@ -52,6 +68,12 @@ function treatLetter(input) {
     console.log(count);  
 }
 
+function displayAnswer() {
+    elem = document.getElementById('word');
+    elem.innerHTML = answer;
+}
+
+// Listener on key strike, launch game and initializes it if it was off, call treatLetter otherwise
 document.addEventListener('keypress', (e) => {
     if (gameIsOn === true) {
         treatLetter(e.key);
