89d27db8e5cff80de2e1ef9f0ca2e00a433f56c1
Maxime Burlat <maximeburlat@gmail.com>
2018-02-22 00:45:24 -0600

    Game is working in console with events!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,70 @@
-function game() {
-    // document.removeEventListener('keypress', game);
-    let word = "BONJOUR";
-    let count = 10;
-    var arrayToComplete = new Array(word.length);
+let gameIsOn = false;
+let word = "BONJOUR";
+let count = 10;
+var arrayToComplete = new Array(word.length);
     for (let index = 0; index < arrayToComplete.length; index++) {
         arrayToComplete[index] = 0;
     }
-    let answer = new String();
+let answer = new String();
 
-    while (count > 0 && answer !== word) {
-        // var letter = prompt("Enter a Letter");
-        function treatLetter(letter) {
-            var newRegular = RegExp(letter);
-            console.log(newRegular);
-            if (word.includes(letter)) {
-                // alert("found it!");
-                indexToStart = 0;
-                stringToSearch = word;
-                while ((indexToStart < word.length) && (stringToSearch.includes(letter))) {
-                    arrayToComplete[stringToSearch.search(newRegular) + indexToStart] = 1;
-                    indexToStart = stringToSearch.search(newRegular) + indexToStart + 1;
-                    stringToSearch = word.slice(indexToStart);
-                }
-                console.log(arrayToComplete);
-                let wordToComplete = new String();
-                for (let index = 0; index < arrayToComplete.length; index++) {
-                    if (arrayToComplete[index] === 1) {
-                        wordToComplete = wordToComplete.concat(word[index]);
-                    } else {
-                        wordToComplete = wordToComplete.concat("_");
-                    }
-                }
-                console.log(wordToComplete);
-                answer = wordToComplete;
-            } else {
-                alert("missed");
-            }
-            count--;
-            console.log(count);  
-        }
-        
-        // document.addEventListener('keypress',(e) => {
-        //     treatLetter(e.key);
-        // });
-        
-    }
-
-    if (count >= 0 && answer === word) {
+function hasGameEnded(globalCount) {
+    if (globalCount >= 0 && answer === word) {
         console.log("You won!");
-    } else {
+        gameIsOn = false;
+    } else if (globalCount === 0){
         console.log("You lost :(");
+        gameIsOn = false;
+    } else {
+        gameIsOn = true;
     }
-
 }
 
-document.addEventListener('keypress', game);
+function treatLetter(input) {
+    letter = input.toUpperCase();
+    console.log(letter);
+    var newRegular = RegExp(letter);
+    console.log(newRegular);
+    if (word.includes(letter)) {
+        // alert("found it!");
+        indexToStart = 0;
+        stringToSearch = word;
+        while ((indexToStart < word.length) && (stringToSearch.includes(letter))) {
+            arrayToComplete[stringToSearch.search(newRegular) + indexToStart] = 1;
+            indexToStart = stringToSearch.search(newRegular) + indexToStart + 1;
+            stringToSearch = word.slice(indexToStart);
+        }
+        console.log(arrayToComplete);
+        let wordToComplete = new String();
+        for (let index = 0; index < arrayToComplete.length; index++) {
+            if (arrayToComplete[index] === 1) {
+                wordToComplete = wordToComplete.concat(word[index]);
+            } else {
+                wordToComplete = wordToComplete.concat("_");
+            }
+        }
+        console.log(wordToComplete);
+        answer = wordToComplete;
+    } else {
+        console.log("missed");
+    }
+    count--;
+    hasGameEnded(count);
+    console.log(count);  
+}
+
+document.addEventListener('keypress', (e) => {
+    if (gameIsOn === true) {
+        treatLetter(e.key);
+    } else {
+        gameIsOn = true;
+        count = 10;
+        for (let index = 0; index < arrayToComplete.length; index++) {
+            arrayToComplete[index] = 0;
+        }
+        answer = "";
+        alert("Game is on!");
+    }
+});
 
 
 
